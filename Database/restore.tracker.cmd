a364712794074dbbbf1cac70df28b88c898648b0
Paul Welter <pwelter@loresoft.com>
2018-03-30 10:04:46 -0500

    fix db scripts

--- Database/restore.tracker.cmd
+++ Database/restore.tracker.cmd
@@ -1,1 +1,1 @@
-sqlcmd -S "(local)\SQL2014" -U sa -P Password12! -i "%APPVEYOR_BUILD_FOLDER%\Database\SqlServer\Tracker.sql"
+sqlcmd -S "(local)\SQL2016" -U sa -P Password12! -i "%APPVEYOR_BUILD_FOLDER%\Database\SqlServer\Tracker.sql"
