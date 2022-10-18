4c225fc6ec51b8729609f172251e4aa8d77192c1
Paul Welter <pwelter@loresoft.com>
2022-10-18 13:59:29 -0500

    Update DataQueryLogger.cs

--- src/FluentCommand/DataQueryLogger.cs
+++ src/FluentCommand/DataQueryLogger.cs
@@ -15,6 +15,13 @@ public class DataQueryLogger : IDataQueryLogger
     /// <summary>
     /// Initializes a new instance of the <see cref="DataQueryLogger"/> class.
     /// </summary>
+    public DataQueryLogger() : this(null)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DataQueryLogger"/> class.
+    /// </summary>
     /// <param name="logger">The logger.</param>
     public DataQueryLogger(Action<string> logger)
     {
@@ -35,17 +42,37 @@ public class DataQueryLogger : IDataQueryLogger
         if (command is null)
             throw new ArgumentNullException(nameof(command));
 
+        var output = FormatCommand(command, duration, exception);
+
+        _logger(output);
+    }
+
+    /// <summary>
+    /// Formats the command for logging.
+    /// </summary>
+    /// <param name="command">The command to log.</param>
+    /// <param name="duration">The execution duration.</param>
+    /// <param name="exception">The exception thrown when executing the command.</param>
+    /// <returns>The command formatted as a string</returns>
+    /// <exception cref="System.ArgumentNullException">command</exception>
+    public static string FormatCommand(IDbCommand command, TimeSpan duration, Exception exception)
+    {
+        if (command == null)
+            throw new ArgumentNullException(nameof(command));
+
         var elapsed = duration.TotalMilliseconds;
         var commandType = command.CommandType;
         var commandTimeout = command.CommandTimeout;
         var resultText = exception == null ? "Executed" : "Error Executing";
 
         var buffer = StringBuilderCache.Acquire();
+
         buffer
             .AppendLine($"{resultText} DbCommand ({elapsed}ms) [CommandType='{commandType}', CommandTimeout='{commandTimeout}']")
             .AppendLine(command.CommandText);
 
         const string parameterFormat = "-- {0}: {1} {2} (Size = {3}; Precision = {4}; Scale = {5}) [{6}]";
+
         foreach (IDataParameter parameter in command.Parameters)
         {
             int precision = 0;
@@ -72,6 +99,6 @@ public class DataQueryLogger : IDataQueryLogger
         }
 
         var output = StringBuilderCache.ToString(buffer);
-        _logger(output);
+        return output;
     }
 }
